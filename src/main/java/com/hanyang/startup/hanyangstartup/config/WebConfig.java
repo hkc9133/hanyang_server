06955900614db7df12c805db0d9d966b213f3897
eney <hkc9133@naver.com>
2020-12-12 14:56:57 +0900

    security 수정

--- src/main/java/com/hanyang/startup/hanyangstartup/config/WebConfig.java
+++ src/main/java/com/hanyang/startup/hanyangstartup/config/WebConfig.java
@@ -16,7 +16,7 @@ public class WebConfig implements WebMvcConfigurer {
                 .allowedOrigins("http://210.103.188.119")
                 .allowedMethods("*")
                 .allowedHeaders("*")
-                .exposedHeaders("Access-Control-Allow-Headers", "Authorization"," x-xsrf-token", "Access-Control-Allow-Headers", "Origin", "Accept", "X-Requested-With", "Content-Type", "Access-Control-Request-Method", "Access-Control-Request-Headers")
+//                .exposedHeaders("Access-Control-Allow-Headers", "Authorization"," x-xsrf-token", "Access-Control-Allow-Headers", "Origin", "Accept", "X-Requested-With", "Content-Type", "Access-Control-Request-Method", "Access-Control-Request-Headers")
                 .maxAge(3600)
                 .allowCredentials(true);
     }
