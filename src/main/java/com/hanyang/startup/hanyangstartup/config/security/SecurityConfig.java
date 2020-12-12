eb2b401c1be8055d39d08355f26551516b803bc5
eney <hkc9133@naver.com>
2020-12-12 13:04:10 +0900

    security 수정

--- src/main/java/com/hanyang/startup/hanyangstartup/config/security/SecurityConfig.java
+++ src/main/java/com/hanyang/startup/hanyangstartup/config/security/SecurityConfig.java
@@ -35,7 +35,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
 
-        http.csrf().disable().cors().configurationSource(corsConfigurationSource()).and()
+        http.cors().configurationSource(corsConfigurationSource()).and().csrf().disable()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
                 .httpBasic()
